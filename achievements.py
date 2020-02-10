e707fb794fe1ad7382a201157058e6d0045656fe
Victor Gambier <victor.gambier@imt-atlantique.net>
2020-02-10 16:39:38 +0100

    Added command interface

--- achievements.py
+++ achievements.py
@@ -15,9 +15,9 @@ achievement_list[milestone_names[60]] = False
 achievement_list[milestone_names[96]] = False
 achievement_list[milestone_names[180]] = False
 
+
 # If the input achievement exists and is unearned, earns the achievement
 def earn_unearned_achievement(achievement_name):
-
     if achievement_name in achievement_list and not achievement_list[achievement_name]:
-            print(f"Achievement unlocked : {achievement_name}")
-            achievement_list[achievement_name] = True
+        print(f"Achievement unlocked : {achievement_name}")
+        achievement_list[achievement_name] = True
--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import math
 import random as rd
 import time
 import sys
-from achievements import earn_unearned_achievement, milestone_names
+from achievements import earn_unearned_achievement, milestone_names, achievement_list
 from strings import logo, morse
 
-# Variables
+# Variables and functions
 
 credits_ects = 0
 
 # Intro cutscene
 
-print("Bienvenue à l'expérience immersive de...")
+print("Bienvenue dans l'expérience immersive de...")
 time.sleep(1)
 for line in logo:
     print(line)
@@ -28,17 +28,31 @@ for c in "C L I C K E R":
     sys.stdout.flush()
     time.sleep(0.25)
 time.sleep(1)
-print("\nAppuyez sur Entrée pour cliquer :)")
+print("\nAppuyez sur Entrée pour cliquer. Tapez help pour une liste de commandes.")
 
 # Game logic
 
 while True:
-    input()
-    credits_ects+=1
+
+    user_input = "default"
+    while user_input != "":
+        user_input = input("> ")
+        if user_input == "help":
+            print('''List of commands:\n\thelp: display this message\n\tachievements: view achievements''')
+        elif user_input == "achievements":
+            print([key if achievement_list[key] else "???" for key in achievement_list])
+        elif user_input == "click":
+            print("Nan, pour cliquer faut juste faire entrée sans rien écrire")
+        elif user_input != "":
+            print("Invalid command. Did you mean \"click\"?")
+
+    credits_ects += 1
     print(f"Vous avez {credits_ects} crédits ECTS!")
-    if rd.randint(0,999) == 0:
+
+    if rd.randint(0, 999) == 0:
         print(morse)
         print("Vous avez trouvé un morse ! Curieux.")
         earn_unearned_achievement("Morse")
-    if (credits_ects in milestone_names):
+
+    if credits_ects in milestone_names:
         earn_unearned_achievement(milestone_names[credits_ects])
