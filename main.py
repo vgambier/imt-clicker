ca1e1f564ad35c71faa6e6dfdc26203e3ea7f741
Victor Gambier <38285114+VeganHunter@users.noreply.github.com>
2020-02-10 11:48:39 +0100

    Added achievements dictionary for cleaner code
    merci pivou

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import math
 import random as rd
 print("Bienvenue à l'expérience immersive de IMT clicker!\nAppuyer sur Entrée pour cliquer :)")
 credits_ects = 0
-has_seen_morse = False
+
+achievements = dict()
+achievements["Morse"] = False
 morse = r"""
                __ ___
              .'. -- . '.
@@ -22,11 +24,11 @@ while True:
     input()
     credits_ects+=1
     print(f"Vous avez {credits_ects} crédits ECTS!")
-    if rd.randint(0,10000) == 0:
+    if rd.randint(0,1) == 0:
         print(morse)
         print("Vous avez trouvé un morse! Curieux.")
-        if not has_seen_morse:
-            print(f"Achievment unlocked : Trouver un morse")
-            has_seen_morse = True
+        if not achievements["Morse"]:
+            print("Achievment unlocked : Trouver un morse")
+            achievements["Morse"] = True
     if (math.log10(credits_ects)%1==0):
       print(f"Achievement unlocked : Obtenir {credits_ects} crédits !")
