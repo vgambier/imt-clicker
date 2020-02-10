0cbde64b704cb9b1b3f2de1244be5cd8a9d008e8
Victor Gambier <38285114+VeganHunter@users.noreply.github.com>
2020-02-10 11:29:00 +0100

    Added achievement system, upgraded to Python 3

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import math
 print("Bienvenue à l'expérience immersive de IMT clicker!\nAppuyer sur Entrée pour cliquer :)")
 credits_ects = 0
 while True:
-    raw_input()
+    input()
     credits_ects+=1
-    print("Vous avez {} crédits ECTS!".format(credits_ects))
+    print(f"Vous avez {credits_ects} crédits ECTS!")
+    if (math.log10(credits_ects)%1==0):
+      print(f"Achievement unlocked : Obtenir {credits_ects} crédits !")
