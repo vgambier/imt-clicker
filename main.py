cf3737d868d88c9bf38ba440ba6044d1163e7b9f
Victor Gambier <38285114+VeganHunter@users.noreply.github.com>
2020-02-10 11:27:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Bienvenue à l'expérience immersive de IMT clicker!\nAppuyer sur Entrée pour cliquer :)")
+credits_ects = 0
+while True:
+    raw_input()
+    credits_ects+=1
+    print("Vous avez {} crédits ECTS!".format(credits_ects))
