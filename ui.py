28e0a80c3734297e3404f114573278d017088818
= <pierre-victor.besson@inria.fr>
2020-02-13 11:24:57 +0100

    centered score, added our lord and savior

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QApplication
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui
 import sys
+
 
 
 class MyWidget(QWidget):
@@ -10,10 +11,25 @@ class MyWidget(QWidget):
             print("No game object found, probably debug mode")
         self.game_object = game_object
         self.layout = QVBoxLayout()
+        ##TODO : set widget size dynamically, depending on image size
+        button_stylesheet = """ 
+            QWidget{
+                color: white; 
+                height: 186px;
+                width: 280px;
+                background-image: url("./src/button_icon.jpg"); 
+                background-repeat: no-repeat; 
+                background-position: center;
+                border: none;
+            }
+        """
+
         self.credit_button = QPushButton('Click')
+        self.credit_button.setStyleSheet(button_stylesheet)
         self.layout.addWidget(self.credit_button)
         self.credit_button.clicked.connect(self.on_click)
         self.score_label = QLabel(str(self.game_object.credits_ects))
+        self.score_label.setAlignment(QtCore.Qt.AlignCenter)
         self.layout.addWidget(self.score_label)
         self.setLayout(self.layout)
         self.show()
