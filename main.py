2bd38eb04b7fd84184b4dfa599018bfd53139586
Gérard Majax <Majax_Gerard@protonmail.com>
2020-02-17 15:31:28 +0100

    added rudimentary upgrade system

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 import sys
 import argparse
 from pynput.keyboard import KeyCode
-
 from achievements import earn, milestones, achievement_list
 from strings import imt_logo, morse, mit_logo
 from pynput import keyboard
@@ -51,6 +50,7 @@ class Game:
 
         # Variables
         self.credits_ects = 0
+        self.professor = 0 #note : might need a better structure in the future as more upgrades get addedN
         self.enter_pressed = False
         self.game_over = False
 
@@ -62,6 +62,20 @@ class Game:
             self.start_game_gui_mode()
         else:
             self.start_game_text_mode()
+
+    def get_n_credits(self, n):
+        self.credits_ects += n
+        if self.credits_ects in milestones: #note: broken in current state since we are likely to go over milestones
+            earn(milestones[self.credits_ects])
+
+    def buy_a_professor(self, professor_price=10): #should probably store professor price somewhere
+        assert(self.credits_ects>=10)
+        self.credits_ects = self.credits_ects - professor_price
+        self.professor += 1
+
+    def earn_upgrade_credits(self): #called every X seconds in main
+        self.credits_ects += self.professor
+        print("Vos profs ont bien travaillé!")
 
     def get_a_credit(self):
         self.credits_ects += 1
@@ -100,13 +114,16 @@ class Game:
         else:
             print("Invalid command. Did you mean \"c\" (click)?")
 
+
     def start_game_gui_mode(self):
+
         game_ui = ui.launch_widget(self)
+
         # create game ui which takes the game as input when button is clicked, you can handle everything in the ui
         # itself by calling the appropriate game functions over there
-
         while not self.game_over:
             pass
+
         print("Game finished!")
 
     # Game logic
@@ -126,3 +143,4 @@ if __name__ == '__main__':
                         help="launches in debug mode (skips intro)")
     args = parser.parse_args()
     Game(args.gui, args.debug)
+
--- ui.py
+++ ui.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLa
 from PyQt5 import QtCore, QtGui
 import sys
 from PIL import Image
+from timeloop import Timeloop
+from datetime import timedelta
 
 class MyWidget(QWidget):
     def __init__(self, game_object=None):
@@ -30,17 +32,42 @@ class MyWidget(QWidget):
         self.credit_button = QPushButton('Click')
         self.credit_button.setStyleSheet(button_stylesheet)
         self.layout.addWidget(self.credit_button)
-        self.credit_button.clicked.connect(self.on_click)
+        self.credit_button.clicked.connect(self.on_credit_click)
         self.score_label = QLabel(str(self.game_object.credits_ects))
         self.score_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.professor_label = QLabel(str(self.game_object.professor) + " profs!")
+        self.professor_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.buy_professor_button = QPushButton("Adopte un prof!")
+        self.buy_professor_button.clicked.connect(self.on_buy_professor_click)
+        self.layout.addWidget(self.buy_professor_button)
         self.layout.addWidget(self.score_label)
+        self.layout.addWidget(self.professor_label)
         self.setLayout(self.layout)
+        tl = Timeloop()
+        @tl.job(interval=timedelta(seconds=10))
+        def sample_job_every_10s():
+            self.earn_upgrade_credits()
+        tl.start()
+
         self.show()
 
-    def on_click(self):
+    def on_credit_click(self):
         self.game_object.get_a_credit()
         self.score_label.setText(str(self.game_object.credits_ects))
         QApplication.processEvents()
+
+    def on_buy_professor_click(self):
+        if self.game_object.credits_ects >= 10:
+            self.game_object.buy_a_professor()
+            self.score_label.setText(str(self.game_object.credits_ects))
+            self.professor_label.setText(str(self.game_object.professor) + " profs!")
+            QApplication.processEvents()
+        else:
+            print("Not enough credits!")
+
+    def earn_upgrade_credits(self):
+        self.game_object.earn_upgrade_credits()
+        self.score_label.setText(str(self.game_object.credits_ects))
 
     def closeEvent(self, *args, **kwargs):
         QtCore.QCoreApplication.processEvents()
